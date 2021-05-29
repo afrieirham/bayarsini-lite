d218366d44ba79fe8b0c84d3d0726c95ac83ccc2
Afrie Irham <itsafrie@gmail.com>
2021-05-29 16:53:59 +0800

    Create GetMyLinkButton as component

---
+++ components/GetMyLinkButton.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import { Button, useClipboard } from '@chakra-ui/react'
+import { Check, Copy } from 'react-feather'
+
+import { useAuth } from '@utils/auth'
+
+function GetMyLinkButton() {
+  const { user } = useAuth()
+  const [origin, setOrigin] = useState('')
+  const { hasCopied, onCopy } = useClipboard(`${origin}/${user?.username}`)
+
+  useEffect(() => {
+    setOrigin(window.origin)
+  }, [window])
+
+  if (hasCopied) {
+    return (
+      <Button
+        mt={2}
+        rightIcon={<Check size='15' />}
+        width='full'
+        size='lg'
+        fontSize='sm'
+        variant='solid'
+        colorScheme='green'
+      >
+        Copied
+      </Button>
+    )
+  }
+
+  return (
+    <Button
+      mt={2}
+      rightIcon={<Copy size='15' />}
+      width='full'
+      size='lg'
+      fontSize='sm'
+      variant='solid'
+      color='white'
+      backgroundColor='gray.900'
+      _hover={{ bg: 'gray.700' }}
+      _focus={{ bg: 'gray.700' }}
+      _active={{ bg: 'gray.800' }}
+      onClick={onCopy}
+    >
+      Get Payment Link
+    </Button>
+  )
+}
+
+export default GetMyLinkButton
--- components/HomeEmptyState.js
+++ components/HomeEmptyState.js
@@ -1,18 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Box, Button, Flex, Heading, Text, useClipboard } from '@chakra-ui/react'
-import { Check, Copy } from 'react-feather'
 
 import { useAuth } from '@utils/auth'
 import DashboardShell from './DashboardShell'
+import GetMyLinkButton from './GetMyLinkButton'
 
 export default function HomeEmptyState() {
   const { user, signOut } = useAuth()
-  const [origin, setOrigin] = useState('')
-  const { hasCopied, onCopy } = useClipboard(`${origin}/${user?.username}`)
-
-  useEffect(() => {
-    setOrigin(window.origin)
-  }, [window])
 
   return (
     <DashboardShell title='Home'>
@@ -33,36 +27,7 @@ export default function HomeEmptyState() {
         <Text mt={2} fontSize='sm' color='gray.500'>
           You have not receive any money. Share your link to request for payment.
         </Text>
-        {hasCopied ? (
-          <Button
-            mt={4}
-            rightIcon={<Check size='15' />}
-            width='full'
-            size='lg'
-            fontSize='sm'
-            variant='solid'
-            colorScheme='green'
-          >
-            Copied
-          </Button>
-        ) : (
-          <Button
-            mt={4}
-            rightIcon={<Copy size='15' />}
-            width='full'
-            size='lg'
-            fontSize='sm'
-            variant='solid'
-            color='white'
-            backgroundColor='gray.900'
-            _hover={{ bg: 'gray.700' }}
-            _focus={{ bg: 'gray.700' }}
-            _active={{ bg: 'gray.800' }}
-            onClick={onCopy}
-          >
-            Get Payment Link
-          </Button>
-        )}
+        <GetMyLinkButton />
         <Button mt={4} variant='ghost' onClick={() => signOut()}>
           Logout
         </Button>
