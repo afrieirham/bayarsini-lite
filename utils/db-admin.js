16528c7c0bceebc99464546afffc28a482ed25ea
Afrie Irham <itsafrie@gmail.com>
2021-05-30 17:24:19 +0800

    Repo cleanup

--- utils/db-admin.js
+++ utils/db-admin.js
@@ -1,34 +1,6 @@
 import { compareDesc, parseISO } from 'date-fns'
 import { db } from '@lib/firebase-admin'
 
-export async function getUserOrders(uid) {
-  const snapshot = await db.collection('orders').where('ownerId', '==', uid).get()
-
-  const orders = []
-  snapshot.forEach((doc) => {
-    orders.push({ id: doc.id, ...doc.data() })
-  })
-
-  const sortedOrders = orders.sort((a, b) =>
-    compareDesc(parseISO(a.createdAt), parseISO(b.createdAt)),
-  )
-
-  return { orders: sortedOrders }
-}
-
-export async function getOrder(orderId, ownerId) {
-  const doc = await db.collection('orders').doc(orderId).get()
-
-  // Handle public request
-  if (!ownerId) {
-    const { customer: _, ...publicOrder } = { id: doc.id, ...doc.data() }
-    return { order: publicOrder }
-  }
-
-  const order = { id: doc.id, ...doc.data() }
-  return { order }
-}
-
 export async function getUserByUsername(username) {
   const query = await db.collection('users').where('username', '==', username).get()
 
--- utils/db.js
+++ utils/db.js
@@ -1,12 +1,6 @@
 import firebase from '@lib/firebase'
 
 const firestore = firebase.firestore()
-const app = firebase.app()
-
-export async function getPaymentConfig(userId) {
-  const doc = await firestore.collection('users').doc(userId).get()
-  return { ...doc.data().toyyibpay }
-}
 
 export async function getFirestoreUser(userId) {
   const doc = await firestore.collection('users').doc(userId).get()
@@ -22,15 +16,6 @@ export async function checkIfUsernameAvailable(username) {
 export function saveUser(user) {
   return firestore.collection('users').doc(user.uid).set(user, { merge: true })
 }
-
-export function createOrder(order) {
-  return firestore.collection('orders').doc(order.id).set(order, { merge: true })
-}
-
-export function updateOrder(orderId, newValues) {
-  return firestore.collection('orders').doc(orderId).update(newValues)
-}
-
 export function updateUserProfile(userId, newValues) {
   return firestore.collection('users').doc(userId).update(newValues)
 }
