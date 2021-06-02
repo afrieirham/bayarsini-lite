bdbb78e12074cedc2596508384f2ba4fb1033519
Afrie Irham <itsafrie@gmail.com>
2021-06-02 13:37:53 +0800

    Add google analytics to track pageView

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
+import { useEffect } from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
-import { customTheme } from '@styles/theme'
-import { AuthProvider } from '@utils/auth'
+import { useRouter } from 'next/router'
 import { DefaultSeo } from 'next-seo'
 
 import SEO from 'next-seo.config'
+import { pageView } from '@utils/gtag'
+import { AuthProvider } from '@utils/auth'
+import { customTheme } from '@styles/theme'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageView(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => router.events.off('routeChangeComplete', handleRouteChange)
+  }, [router, router.events])
+
   return (
     <ChakraProvider theme={customTheme}>
       <AuthProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+import { GA_TRACKING_ID } from '@utils/gtag'
+
 class MyDocument extends Document {
   render() {
     return (
@@ -18,6 +20,21 @@ class MyDocument extends Document {
             rel='stylesheet'
           />
           <link rel='icon' href='/favicon.ico' />
+
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
