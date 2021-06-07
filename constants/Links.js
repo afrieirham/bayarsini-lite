8bfeb40d636a63f8f309928213d3711ac47d3659
Afrie Irham <itsafrie@gmail.com>
2021-06-07 12:52:03 +0800

    Fix typo in registration guide link

--- constants/Links.js
+++ constants/Links.js
@@ -1,6 +1,6 @@
 export const FAQ =
   'https://docs.google.com/document/u/4/d/e/2PACX-1vRZAZQJxjeotII-Adr485wnUu8192ra7gsFYkb6YW3hTdTQw76hDuTMumUaVnUjIDjBaS1yOlCH1cX4/pub'
 export const RegistrationGuide =
-  'https://docs.google.com/document/u/4/d/e/2PACX-1vR9i-U_m3ZwtddP-JqSW7UwP7mAAbEY1CmftijRZQpE56R2gMpBMv8SVPkrNOmoc_pIhEYFQZXwEv6W/pub`'
+  'https://docs.google.com/document/u/4/d/e/2PACX-1vR9i-U_m3ZwtddP-JqSW7UwP7mAAbEY1CmftijRZQpE56R2gMpBMv8SVPkrNOmoc_pIhEYFQZXwEv6W/pub'
 export const ToyyibpayAffiliate = 'https://toyyibpay.com/e/9168067289423942'
 export const FeedbackLink = 'https://forms.gle/EzD5bFvHNf8C73nY6'
