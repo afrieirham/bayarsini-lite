431207ca411ab3bef4fad06501dff2fb1fa955c9
Afrie Irham <itsafrie@gmail.com>
2021-06-13 20:15:26 +0800

    Use notion for FAQs and RegistrationGuide

--- constants/Links.js
+++ constants/Links.js
@@ -1,6 +1,5 @@
-export const FAQ =
-  'https://docs.google.com/document/u/4/d/e/2PACX-1vRZAZQJxjeotII-Adr485wnUu8192ra7gsFYkb6YW3hTdTQw76hDuTMumUaVnUjIDjBaS1yOlCH1cX4/pub'
+export const FAQ = 'https://www.notion.so/FAQs-f84cf5bb644f41c5b72cdaeab8c3d4d6'
 export const RegistrationGuide =
-  'https://docs.google.com/document/u/4/d/e/2PACX-1vR9i-U_m3ZwtddP-JqSW7UwP7mAAbEY1CmftijRZQpE56R2gMpBMv8SVPkrNOmoc_pIhEYFQZXwEv6W/pub'
+  'https://www.notion.so/Registration-Guide-7853b3de1e644c3c976b8ed36734e845'
 export const ToyyibpayAffiliate = 'https://toyyibpay.com/e/9168067289423942'
 export const FeedbackLink = 'https://forms.gle/EzD5bFvHNf8C73nY6'
